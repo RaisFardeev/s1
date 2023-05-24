12b8978e8124de47d67b65251af55465aa21d1fd
RaisFardeev <raisfardeev5@gmail.com>
2023-05-24 18:41:43 +0300

    + tests

--- app/tests/test_api.py
+++ app/tests/test_api.py
@@ -1,8 +1,13 @@
 def test_api_ads(app, client):
     res = client.get('/api/v1/get_ads')
-    assert res.satus_code == 200
+    assert res.status_code == 200
 
 
 def test_api_users(app, client):
     res = client.get('/api/v1/get_users')
-    assert res.satus_code == 302
+    assert res.status_code == 200
+
+
+def test_api_user(app, client):
+    res = client.get('/api/v1/get_users/abcdefg@mail.ru')
+    assert res.status_code == 404
--- app/tests/test_crud.py
+++ app/tests/test_crud.py
@@ -1,3 +1,30 @@
-def test_create(app, client):
-    res = client.get('/ad/create')
-    assert res.satus_code == 404
+from .. import db
+
+
+def test_registration(app, client):
+    data = dict(email='rfgfdgfgf@mail.ru', name='asddsdfdv', password1='qw', password2='qw')
+    res = client.post('/registrate', data=data)
+    assert res.status_code == 200
+
+
+def test_login(app, client):
+    data = dict(email='rfgfdfgf@mail.ru', password='qw')
+    res = client.post('/login', data=data)
+    assert res.status_code == 200
+
+
+def test_edit_profile(app, client):
+    data = dict(email='rfgfdgfgf@mail.ru', name='asddsdfdv', old_password='qw', new_password1='qw12', new_password2='qw12')
+    res = client.post('/editprofile', data=data)
+    assert res.status_code == 302
+
+
+def test_create_ad(app, client):
+    data = dict(name='asddsdfdv', description='adfeffsdce', category='kids', price='21')
+    res = client.post('/ad/create', data=data)
+    assert res.status_code == 302
+
+
+def test_ad_delete(app, client):
+    res = client.get('/ad/1/delete')
+    assert res.status_code == 302
