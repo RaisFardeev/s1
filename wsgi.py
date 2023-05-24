6ac1bb0cd37f7d17726d39824878e530d584667c
RaisFardeev <raisfardeev5@gmail.com>
2023-05-24 18:42:16 +0300

    !

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,4 @@
-from app import app  # , socket
+from app import app, socket
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000, debug=True)
-    # socket.run(app=app, host='127.0.0.1', port=5000, debug=True, allow_unsafe_werkzeug=True)
+    socket.run(app=app, host='127.0.0.1', port=5000, debug=True, allow_unsafe_werkzeug=True)
